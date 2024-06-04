4f06510a157b6874492be738ded22006d8dd2036
adrifoster <adrifost@ucar.edu>
2024-06-04 10:19:37 -0600

    fix array

--- biogeochem/FatesPatchMod.F90
+++ biogeochem/FatesPatchMod.F90
@@ -352,7 +352,7 @@ module FatesPatchMod
       this%fabi(:)                      = nan
       this%sabs_dir(:)                  = nan 
       this%sabs_dif(:)                  = nan
-      this%pyrogenic_carbon             = nan
+      this%pyrogenic_carbon(:)          = nan
       
       ! ROOTS
       this%btran_ft(:)                  = nan 
@@ -435,7 +435,7 @@ module FatesPatchMod
       this%fabd(:)                           = 0.0_r8
       this%sabs_dir(:)                       = 0.0_r8
       this%sabs_dif(:)                       = 0.0_r8
-      this%pyrogenic_carbon                  = 0.0_r8
+      this%pyrogenic_carbon(:)               = 0.0_r8
 
       ! ROOTS
       this%btran_ft(:)                       = 0.0_r8
